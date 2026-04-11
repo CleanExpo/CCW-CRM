0c9183f32a91a3cbfe6199106d47aecd7e15d8cc
Rana Muzamil <mmlrana00@gmail.com>
2026-04-11 10:54:25 +0500

    feat(app): add marketing-pages route group layout

---
+++ src/app/(marketing-pages)/layout.tsx
@@ -0,0 +1,5 @@
+import { MarketingSiteChrome } from '@/components/landing/marketing-site-chrome';
+
+export default function MarketingPagesLayout({ children }: { children: React.ReactNode }) {
+  return <MarketingSiteChrome>{children}</MarketingSiteChrome>;
+}
