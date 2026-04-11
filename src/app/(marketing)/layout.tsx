c4149c2da5aae2373d095ba68eb153f13a7573fc
Rana Muzamil <mmlrana00@gmail.com>
2026-04-11 10:54:42 +0500

    feat(app): add marketing layout with MarketingSiteChrome

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,5 @@
+import { MarketingSiteChrome } from '@/components/landing/marketing-site-chrome';
+
+export default function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return <MarketingSiteChrome>{children}</MarketingSiteChrome>;
+}
