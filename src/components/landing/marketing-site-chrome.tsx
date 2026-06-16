663b98ab712863e1dcaea5dc77bebc24c28c406b
Rana Muzamil <mmlrana00@gmail.com>
2026-06-17 01:26:10 +0500

    Mark marketing site chrome as a client component so ambient and header render correctly.

--- src/components/landing/marketing-site-chrome.tsx
+++ src/components/landing/marketing-site-chrome.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { MarketingAmbientCanvas } from '@/components/landing/marketing-ambient';
 import { marketingFont } from '@/components/landing/marketing-font';
 import { MarketingFooter } from '@/components/landing/marketing-footer';
