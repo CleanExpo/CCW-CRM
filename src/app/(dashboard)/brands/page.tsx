6b436fa5f3afe5e29fbc700a2d38a77013cc6534
Rana Muzamil <mmlrana00@gmail.com>
2026-07-10 17:15:36 +0500

    Add brands browse page for synced Cin7 master data.

---
+++ src/app/(dashboard)/brands/page.tsx
@@ -0,0 +1,5 @@
+import { Cin7ReferenceListPage } from '@/components/integrations/Cin7ReferenceListPage';
+
+export default function Cin7BrandsPage() {
+  return <Cin7ReferenceListPage entity="brands" />;
+}
