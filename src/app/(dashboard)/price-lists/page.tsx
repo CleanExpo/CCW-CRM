8226d9911fdc60eb5d26ed55d5839347f1b56968
Rana Muzamil <mmlrana00@gmail.com>
2026-07-10 17:15:37 +0500

    Add price lists browse page for synced Cin7 master data.

---
+++ src/app/(dashboard)/price-lists/page.tsx
@@ -0,0 +1,5 @@
+import { Cin7ReferenceListPage } from '@/components/integrations/Cin7ReferenceListPage';
+
+export default function Cin7PriceListsPage() {
+  return <Cin7ReferenceListPage entity="price-lists" />;
+}
