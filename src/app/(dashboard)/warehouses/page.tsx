123e9a15771c064a266fc3ff1cc0b0e111481e38
Rana Muzamil <mmlrana00@gmail.com>
2026-07-10 17:15:50 +0500

    Add warehouses browse page for synced Cin7 branch locations.

---
+++ src/app/(dashboard)/warehouses/page.tsx
@@ -0,0 +1,5 @@
+import { Cin7ReferenceListPage } from '@/components/integrations/Cin7ReferenceListPage';
+
+export default function Cin7WarehousesPage() {
+  return <Cin7ReferenceListPage entity="warehouses" />;
+}
