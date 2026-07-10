2f39fc3c04a3e4948219e8e0e24bb24b3ad85d2a
Rana Muzamil <mmlrana00@gmail.com>
2026-07-10 17:15:37 +0500

    Add tax codes browse page for synced Cin7 master data.

---
+++ src/app/(dashboard)/tax-codes/page.tsx
@@ -0,0 +1,5 @@
+import { Cin7ReferenceListPage } from '@/components/integrations/Cin7ReferenceListPage';
+
+export default function Cin7TaxCodesPage() {
+  return <Cin7ReferenceListPage entity="tax-codes" />;
+}
