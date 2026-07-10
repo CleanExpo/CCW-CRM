e8325113d8a692309a27748b10d2d0bf68c5230d
Rana Muzamil <mmlrana00@gmail.com>
2026-07-10 17:15:36 +0500

    Add product categories browse page for synced Cin7 master data.

---
+++ src/app/(dashboard)/product-categories/page.tsx
@@ -0,0 +1,5 @@
+import { Cin7ReferenceListPage } from '@/components/integrations/Cin7ReferenceListPage';
+
+export default function Cin7ProductCategoriesPage() {
+  return <Cin7ReferenceListPage entity="product-categories" />;
+}
