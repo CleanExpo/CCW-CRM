26b742c33f89989f75344d12de758e3da9d0a31b
Rana Muzamil <mmlrana00@gmail.com>
2026-07-10 17:15:48 +0500

    Add units of measure browse page for synced Cin7 master data.

---
+++ src/app/(dashboard)/units-of-measure/page.tsx
@@ -0,0 +1,5 @@
+import { Cin7ReferenceListPage } from '@/components/integrations/Cin7ReferenceListPage';
+
+export default function Cin7UnitsOfMeasurePage() {
+  return <Cin7ReferenceListPage entity="units-of-measure" />;
+}
