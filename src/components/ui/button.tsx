3cb4a8fde8b8d4bd09f8c5d21c69aad5e7ee0a80
Rana Muzamil <mmlrana00@gmail.com>
2026-08-23 15:34:29 +0500

    Pass children through Button asChild so link labels like Export B1 CSV actually reach the DOM.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -126,7 +126,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           className={cn(buttonVariants({ variant, size, className }))}
           ref={ref}
           {...props}
-        />
+        >
+          {children}
+        </Comp>
       );
     }
 
